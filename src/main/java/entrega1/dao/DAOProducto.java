0b5ab3138c28999f8c4f5d6456554353a6f8c2d0
Simon <xsub123@gmail.com>
2025-08-30 22:32:55 -0300

    Implementado DAO de la entidad Producto

--- src/main/java/entrega1/dao/DAOProducto.java
+++ src/main/java/entrega1/dao/DAOProducto.java
@@ -1,4 +1,12 @@
 package entrega1.dao;
 
+import entrega1.entities.Cliente;
+import entrega1.entities.Producto;
+import entrega1.repository.mysql.ProductoDAO;
+
 public interface DAOProducto {
+    public Producto get (int id);
+    public void insert (Producto nuevoProducto);
+    public void update (Producto nuevoProducto);
+    public void delete (int id);
 }
--- src/main/java/entrega1/entities/Producto.java
+++ src/main/java/entrega1/entities/Producto.java
@@ -1,4 +1,42 @@
 package entrega1.entities;
 
+import entrega1.repository.mysql.ProductoDAO;
+
 public class Producto {
+    private int idProducto;
+    private String nombre;
+    private float valor;
+
+
+    public Producto(){
+
+    }
+    public Producto(String nombre, float valor){
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
