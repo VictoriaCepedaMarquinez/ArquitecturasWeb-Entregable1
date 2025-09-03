6cdfaadb88b9d295e153dcdeefc98b4355ee129b
JulioTudai <Juliotwete@hotmail.com>
2025-09-03 20:24:04 -0300

    Entiti y DAO Factura Producto terminado, consultar get de factura Producto inyeccion de otros daos

--- src/main/java/entrega1/entities/FacturaProducto.java
+++ src/main/java/entrega1/entities/FacturaProducto.java
@@ -1,4 +1,40 @@
 package entrega1.entities;
 
 public class FacturaProducto {
+
+    private Factura idFactura;
+    private Producto idProducto;
+    private int cantidad;
+
+    public FacturaProducto(Factura idFactura, Producto idProducto, int cantidad) {
+        this.idFactura = idFactura;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+    }
+
+    public FacturaProducto() {}
+
+    public Factura getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(Factura idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Producto getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Producto idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
