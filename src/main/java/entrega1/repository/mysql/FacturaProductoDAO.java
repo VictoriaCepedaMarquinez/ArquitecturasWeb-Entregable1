0d60da09d97a5f43aa4be3d4e809a5154313ce2f
JulioTudai <Juliotwete@hotmail.com>
2025-09-03 23:09:39 -0300

    Se termino consulta get FacturaProducto, FacturaProducto y DAOFacturaProducto

--- src/main/java/entrega1/repository/mysql/FacturaProductoDAO.java
+++ src/main/java/entrega1/repository/mysql/FacturaProductoDAO.java
@@ -3,6 +3,7 @@ package entrega1.repository.mysql;
 import entrega1.dao.DAOFacturaProducto;
 import entrega1.entities.Factura;
 import entrega1.entities.FacturaProducto;
+import entrega1.entities.Producto;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,27 +18,30 @@ public class FacturaProductoDAO implements DAOFacturaProducto {
         this.cn = cn;
     }
 
+    public FacturaProducto get(int id1, int id2) {
+        final String sql = "SELECT cantidad FROM FacturaProducto WHERE idFactura = ? AND idProducto = ?";
 
-    public FacturaProducto get(int id){
+        try (PreparedStatement ps = cn.prepareStatement(sql)) {
+            ps.setInt(1, id1);
+            ps.setInt(2, id2);
 
-        final String sql = "SELECT idFactura, idProducto, cantidad FROM FacturaProducto WHERE idFactura = ? AND idProducto = ?";
-        try(PreparedStatement ps = cn.prepareStatement(sql)) {
-            ps.setInt(1, id);
-            try(ResultSet rs = ps.executeQuery()) {
-                if(rs.next()) {
-                    FacturaProducto facturaProducto = new FacturaProducto();
-                    facturaProducto.setIdFactura(rs.getInt(1));
-                    facturaProducto.setIdProducto(rs.getInt("idCliente"));
-                    return FacturaProducto;
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    FacturaDAO facturaDAO = new FacturaDAO(cn); //esto podria hacerse con inyeccion para no crear 2
+                    ProductoDAO productoDAO = new ProductoDAO(cn);// preguntar
+
+                    Factura factura = facturaDAO.get(id1);
+                    Producto producto = productoDAO.get(id2);
+
+                    return new FacturaProducto(factura, producto, rs.getInt(3));
                 } else {
                     return null;
                 }
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Error al buscar la Factura del Prodcuto  con id:" + id, e);
+            throw new RuntimeException("Error al buscar FacturaProducto con idFactura="
+                    + id1 + " e idProducto=" + id2, e);
         }
-
-        return null;
     }
 
 
